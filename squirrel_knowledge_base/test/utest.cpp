04a0d0889b5e38f5e5323fdc1da402b356d13053
Michael <michael.cashmore@kcl.ac.uk>
2014-11-05 12:54:35 +0000

    Added unit test files, but still to port over test code.

---
+++ squirrel_knowledge_base/test/utest.cpp
@@ -0,0 +1,24 @@
+#include <gtest/gtest.h>
+
+/* TODO: for all, port over PANDORA knowledge harness */
+
+TEST(SquirrelKnowledgeBase, basicTest){
+  EXPECT_TRUE(true);
+}
+
+TEST(SquirrelKnowledgeBase, knowledgeAddition){
+  EXPECT_TRUE(true);
+}
+
+TEST(SquirrelKnowledgeBase, knowledgeRemoval){
+  EXPECT_TRUE(true);
+}
+
+TEST(SquirrelKnowledgeBase, filterTest){
+  EXPECT_TRUE(true);
+}
+
+int main(int argc, char** argv){
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
