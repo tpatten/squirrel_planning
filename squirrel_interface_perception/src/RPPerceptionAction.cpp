24a6531d9a9a1a5ef2bbfc1877100bb8cf908751
ipa-nhg <nadia.hammoudeh.garcia@ipa.fraunhofer.de>
2015-03-13 15:56:46 +0100

    merge conflict

--- squirrel_interface_perception/src/RPPerceptionAction.cpp
+++ squirrel_interface_perception/src/RPPerceptionAction.cpp
@@ -167,8 +167,7 @@ namespace KCL_rosplan {
 		ros::NodeHandle nh;
 
 		bool simulate;
-		nh.getParam("simulate", simulate);
-		//nh.getParam("simulate_perception", simulate);
+		nh.getParam("simulate_perception", simulate);
 
 		std::string actionserver;
 		nh.param("action_server", actionserver, std::string("/look_for_objects"));
