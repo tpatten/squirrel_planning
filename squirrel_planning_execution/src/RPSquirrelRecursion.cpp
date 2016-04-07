c1722c56216e21eab0c12fbd991c42745be2a2bc
m312z <michael.cashmore@kcl.ac.uk>
2016-04-07 13:20:00 +0100

    upped occ threshold

--- squirrel_planning_execution/src/RPSquirrelRecursion.cpp
+++ squirrel_planning_execution/src/RPSquirrelRecursion.cpp
@@ -649,7 +649,7 @@ namespace KCL_rosplan {
 				tf::Vector3 p2(-0.5, 4.07, 0.00);
 				tf::Vector3 p3(3.49, 0.01, 0.00);
 				tf::Vector3 p4(-0.35, -0.09, 0.00);
-				view_cone_generator->createViewCones(view_poses, bounding_box, 10, 5, 30.0f, 2.0f, 100, 0.5f);
+				view_cone_generator->createViewCones(view_poses, bounding_box, 10, 80, 30.0f, 1.0f, 100, 0.5f);
 			}
 			else
 			{
